c73ac5c7ff97b78ab5b426987296d3361063fe08
FrancoLuvisotti <franco_luvi@hotmail.com>
2024-07-11 13:29:59 -0300

    error tp.py

--- tp.py
+++ tp.py
@@ -22,7 +22,7 @@ class Catalogo:
         self.conn = mysql.connector.connect(
             host = "francoluvi.mysql.pythonanywhere-services.com",
             user = "francoluvi",
-            password ="#4#.$N2ia_ZQf3Lq",
+            password ="4#a*ms?RAY#T7u@",
             # port = "3306",
             database = "indumentaria"
         )
@@ -110,7 +110,7 @@ class Catalogo:
 # Cuerpo del programa
 #--------------------------------------------------------------------
 # Crear una instancia de la clase Catalogo
-catalogo = Catalogo(host='francoluvi.mysql.pythonanywhere-services.com', user='francoluvi', password='#4#.$N2ia_ZQf3Lq', database='francoluvi$indumentaria') #4#.$N2ia_ZQf3Lq
+catalogo = Catalogo(host='francoluvi.mysql.pythonanywhere-services.com', user='francoluvi', password='4#a*ms?RAY#T7u@', database='francoluvi$indumentaria') # 4#.$N2ia_ZQf3Lq o 4#a*ms?RAY#T7u@
 
 # Carpeta para guardar las imagenes.
 RUTA_DESTINO = '/home/francoluvi/static/imagenes'
